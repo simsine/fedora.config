6610c4aa159c04f1deb0dba756a6b61e15a7c4b7
Simen Strømsnes <simen@simsine.no>
2025-04-15 19:52:38 +0200

    Added symlinks for config files

---
+++ symlinks.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+ln -sf ~/git/fedora.config/.config/fish/config.fish ~/.config/fish/config.fish
+ln -sf ~/git/fedora.config/.config/mpv/mpv.conf ~/.config/mpv/mpv.conf
+ln -sf ~/git/fedora.config/.config/btop/btop.conf ~/.config/btop/btop.conf
+ln -sf ~/git/fedora.config/.config/starship.toml ~/.config/starship.toml
+ln -sf ~/git/fedora.config/.config/VSCodium/User/snippets/ ~/.config/VSCodium/User/
+ln -sf ~/git/fedora.config/.config/VSCodium/User/keybindings.json ~/.config/VSCodium/User/keybindings.json
+ln -sf ~/git/fedora.config/.config/VSCodium/User/settings.json ~/.config/VSCodium/User/settings.json
