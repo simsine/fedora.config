8c279f615f6b56e27333662eeb67805c1f4abe6e
Simen Strømsnes <simen@simsine.no>
2025-07-08 14:31:09 +0200

    mhm

--- symlinks.sh
+++ symlinks.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ln -sf ~/git/fedora.config/.config/fish/config.fish ~/.config/fish/config.fish
-ln -sf ~/git/fedora.config/.config/fish/completions ~/.config/fish/completions
+ln -sf ~/git/fedora.config/.config/fish/completions ~/.config/fish/
 ln -sf ~/git/fedora.config/.config/mpv/mpv.conf ~/.config/mpv/mpv.conf
 ln -sf ~/git/fedora.config/.config/btop/btop.conf ~/.config/btop/btop.conf
 ln -sf ~/git/fedora.config/.config/starship.toml ~/.config/starship.toml
