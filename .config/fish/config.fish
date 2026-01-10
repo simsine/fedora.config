54af5822af6c87d72a0a71149a14ae6e54b3c1a4
simsine <simen@simsine.no>
2026-01-10 15:52:06 +0100

    Added atuin for shell history and some other small stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,27 @@
-starship init fish | source
-zoxide init fish | source
+# Initalize on interactive shell
+if status is-interactive
+  [ "$(command -v atuin)" ] && eval "$(starship init fish)"
+  [ "$(command -v atuin)" ] && eval "$(atuin init fish)"
+  [ "$(command -v atuin)" ] && eval "$(zoxide init fish)"
+end
 
+# Aliases
 alias ff="fastfetch"
 alias zola="flatpak run org.getzola.zola"
 alias systui="systemctl-tui"
 alias ls="eza --icons=auto"
-alias lsa="eza -lah --git-ignore --git --icons=auto"
+alias lsa="eza -lah --git-ignore --git --icons=auto --group-directories-first"
 alias cat="bat -p"
 alias grep="rg"
 
+# Env variables
 set NVM_DIR $HOME/.nvm
 set nvm_default_version lts/krypton
 
+set DOCKER_HOST unix:///run/user/$UID/podman/podman.sock
+set PODMAN_COMPOSE_WARNING_LOGS false
+
+# Path variables
 fish_add_path /home/simsine/.dotnet/tools
 
 # pnpm
